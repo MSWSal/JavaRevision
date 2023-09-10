6f42ea3bab6122983a5eeb47bbc790a14112eed0
MSWSal <sahansalgado10@gmail.com>
2023-09-10 23:15:54 +0530

    1st

--- RevNum.java
+++ RevNum.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class RevNum {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter :");
+        int num = sc.nextInt();
+
+        //starts
+        int reverse = 0;
+
+        while (num != 0){
+            reverse *= 10;
+            reverse += num%10;
+            num /=10;
+        }
+
+        System.out.println("Reverse is :"+reverse);
 
     }
 
