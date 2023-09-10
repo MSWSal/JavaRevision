a272c8dc73f76252ad3b8158151b488af3c79007
MSWSal <sahansalgado10@gmail.com>
2023-09-10 23:46:56 +0530

    2

--- RevStr.java
+++ RevStr.java
@@ -1,12 +1,25 @@
 public class RevStr {
     public static void main(String[] args) {
-        //concatanation
         String str = "Hello";
         String rev ="";
+        //concatanation
+//
+//
+//
+//        for(int i=str.length()-1; i>=0;i-- ){
+//            rev = rev + str.charAt(i);
+//        }
 
-        for(int i=str.length()-1; i>=0;i-- ){
-            rev = rev + str.charAt(i);
+        //char[]
+//        char rev[]= new char[0];
+        char arr[]= str.toCharArray();
+
+
+        for (int i=arr.length-1;i>=0;i--){
+            rev+= arr[i];
+
         }
+
 
         System.out.println(rev);
     }
