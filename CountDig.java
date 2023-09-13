9e06d49fab8272ee3337c894d67f811a19dd5e03
MSWSal <sahansalgado10@gmail.com>
2023-09-13 15:43:53 +0530

    1

---
+++ CountDig.java
@@ -0,0 +1,8 @@
+public class CountDig {
+    public static void main(String[] args) {
+        Integer number = 12345;
+        String strnum = String.valueOf(number);
+
+        System.out.println(strnum.length());
+    }
+}
