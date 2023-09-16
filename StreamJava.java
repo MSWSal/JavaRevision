e77e7a1ae8ec2fd4ca3994f78f3117d30e999f7c
MSWSal <sahansalgado10@gmail.com>
2023-09-16 18:17:50 +0530

    2

--- StreamJava.java
+++ StreamJava.java
@@ -1,9 +1,13 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamJava {
     public static void main(String[] args) {
         List<Integer> numarray = Arrays.asList(1,2,3,4);
 
+        List<Integer> even = numarray.stream()
+                .filter(x -> x % 2==0)
+                .collect(Collectors.toList());
     }
 }
