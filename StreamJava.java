b77f1f1cb993bd37ca635b4ccaaa2ae37200a369
MSWSal <sahansalgado10@gmail.com>
2023-09-16 18:15:55 +0530

    1

---
+++ StreamJava.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamJava {
+    public static void main(String[] args) {
+        List<Integer> numarray = Arrays.asList(1,2,3,4);
+
+    }
+}
